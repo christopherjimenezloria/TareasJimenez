d353f508f0e039a0440ec94cb06f4c15ca201224
Christopher J <chr.loria@hotmail.com>
2026-02-27 14:15:33 -0600

    Corrige errores de flake8 en errores.py

--- Tarea1/errores.py
+++ Tarea1/errores.py
@@ -1,5 +1,6 @@
-def funcion_con_errores ( ) :
-    x=   1
-    if x==1:print("Hola")
-    y =  [1 ,2 ,3]
+def funcion_sin_errores():
+    x = 1
+    if x == 1:
+        print("Hola")
+    y = [1, 2, 3]
     return y
